03b1975044de4aaa98d41579bf63af52fd875b21
Mykol Benning <msmykolbenning@gmail.com>
2019-01-22 16:14:50 -0500

    Initial comit

--- assignments/objects.js
+++ assignments/objects.js
@@ -83,6 +83,8 @@ console.log(intern5.gender);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
